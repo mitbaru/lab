2d736fbffc851afd2554b12e75635061faf34205
mitbaru <ihackutod@mail.ru>
2022-12-24 22:51:26 +0200

    Добавлены структуры для хранения данных

--- vved_lab9/session_data.h
+++ vved_lab9/session_data.h
@@ -1,1 +1,30 @@
 #pragma once
+#ifndef BOOK_SUBSCRIPTION_H
+#define BOOK_SUBSCRIPTION_H
+
+#include "constants.h"
+
+struct date
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct person
+{
+    char first_name[MAX_STRING_SIZE];
+    char middle_name[MAX_STRING_SIZE];
+    char last_name[MAX_STRING_SIZE];
+};
+
+struct book_subscription
+{
+    person reader;
+    date start;
+    date finish;
+    person author;
+    char title[MAX_STRING_SIZE];
+};
+
+#endif
