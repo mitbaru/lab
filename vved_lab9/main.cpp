349a38fc00d86333c0dfdd9fc52853dabd594181
mitbaru <ihackutod@mail.ru>
2022-12-24 23:04:58 +0200

    Добавлены тестовые данные

--- vved_lab9/main.cpp
+++ vved_lab9/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #8. Session result\n";
     cout << "Author: Artyom Polityko\n";
+    cout << "Group: 11\n";
     return 0;
 }
