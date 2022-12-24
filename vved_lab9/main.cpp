5cc7dd5709d99c3c361b32bfbb84b63b37395d83
mitbaru <94171661+mitbaru@users.noreply.github.com>
2022-12-24 21:58:50 +0200

    Добавлен вывод общей информации о задании

---
+++ vved_lab9/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #8. Session result\n";
+    cout << "Author: Artyom Polityko\n";
+    return 0;
+}
