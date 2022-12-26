6b2785f915bfb562899de330d69014accb7abeb5
mitbaru <ihackutod@mail.ru>
2022-12-26 22:50:41 +0200

    Внесение некоторых изменений

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../vved_lab9/session_data.h"
+#include "../vved_lab9/processing.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
