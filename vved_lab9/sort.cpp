5e2f6949f00b26f1b8d54287a2b9cd583cc23f21
mitbaru <ihackutod@mail.ru>
2022-12-26 19:14:00 +0200

    Создана функция-заглушка для обработки данных

---
+++ vved_lab9/sort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "sort.h"
+
+session_data** insertion(session_data* array[], int size) {
+
+}
---
+++ vved_lab9/sort.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef SORT_H
+#define SORT_H
+
+#include "session_data.h"
+
+
+
+#endif
